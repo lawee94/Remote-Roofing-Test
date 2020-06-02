0818b1627f6f85e5b4b250d50d14e8933fb93621
lawee94 <omojugbaolawale@gmail.com>
2020-06-03 03:05:21 +1100

    build added

---
+++ build/precache-manifest.d31da812e3ad1061a54dbad0dba94cc9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bd4dc233534a16af0dada4cd0d41f3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "617b685be9a13931e783",
+    "url": "/static/js/2.84630cdc.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.84630cdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9959ed5421a769a0055",
+    "url": "/static/js/main.e2e4a79a.chunk.js"
+  },
+  {
+    "revision": "7feae562d348f4e3c5d2",
+    "url": "/static/js/runtime-main.b534bbbd.js"
+  },
+  {
+    "revision": "e27985164447b1b67b94024beab8cb26",
+    "url": "/static/media/apple.e2798516.png"
+  },
+  {
+    "revision": "e4482a00f9042d4d4165a1cc01a5926a",
+    "url": "/static/media/fc.e4482a00.ico"
+  },
+  {
+    "revision": "e9ebd6c06b8ddca767cff5aadb690703",
+    "url": "/static/media/gp.e9ebd6c0.png"
+  },
+  {
+    "revision": "a73511f736990632b4ba13a2f0673856",
+    "url": "/static/media/movies.a73511f7.PNG"
+  },
+  {
+    "revision": "a799473def21536b1edb8b97b260a5fd",
+    "url": "/static/media/series.a799473d.PNG"
+  },
+  {
+    "revision": "013a48001ddf391502408d5ed860e462",
+    "url": "/static/media/twit.013a4800.ico"
+  }
+]);
